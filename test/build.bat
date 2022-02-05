a3069689510619ba54d6d076ebf549e11d72fada
H. A. L <31424568+halayer@users.noreply.github.com>
2022-02-05 18:05:12 +0100

    Update build.bat

--- test/build.bat
+++ test/build.bat
@@ -1,5 +1,16 @@
 @echo off
 
-gcc test.c ../bus.o -I../include -o test.exe
+gcc test.c ../bus.o -I../include -o c_test.exe
+
+cd ada
+gcc -c test_component.adb -I../../bindings/ada
+gcc -c test.adb -I../../bindings/ada
+gnatbind test -I../../bindings/ada
+gnatlink test ../../bus.o
+del test_component.ali
+del test_component.o
+del test.ali
+del test.o
+cd ..
 
 exit /B
