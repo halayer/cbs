5a9e953fe3b883f44aa9b64197790e2128aa5578
H. A. L <31424568+halayer@users.noreply.github.com>
2022-02-05 18:09:07 +0100

    Update build.bat

--- build.bat
+++ build.bat
@@ -1,6 +1,11 @@
 @echo off
 
-gcc test.c bus.c -o test.exe -Iinclude
+gcc -c bus.c -o bus.o -Iinclude
 gcc bus.c --shared -o bindings/cbs.dll -Iinclude
+
+cd bindings/ada
+gcc -c components.ads
+gcc -c buses.ads
+cd ../..
 
 exit /B
