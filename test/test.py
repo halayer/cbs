0bbc46b5d378270143f4050c5874f52e1fa5adaf
H. A. L <31424568+halayer@users.noreply.github.com>
2022-02-05 18:34:45 +0100

    Update test.py

--- test/test.py
+++ test/test.py
@@ -1,10 +1,27 @@
 import sys
 sys.path.insert(0, "../bindings")
 import cbs
+import ctypes
+
+if sys.platform == "win32":
+    memset = ctypes.cdll.msvcrt.memset
+else:
+    try: memset = ctypes.cdll.libc.memset
+    except: memset = ctypes.CDLL("libc.so").memset
+
+memset.argtypes = [ctypes.c_void_p, ctypes.c_char, ctypes.c_size_t]
+
 
 class Test(cbs.Component):
     def is_in_addr_space(*args): return 1
-    def read(*args): print("py_read"); return 0
+    
+    def read(self, inst, addr, byte_size, dst):
+        print("py_read")
+        
+        memset(dst, ctypes.c_char(0x41), byte_size)
+        
+        return 0
+        
     def write(*args): print("py_write"); return 0
 
 bus = cbs.Bus()
@@ -13,3 +30,5 @@ comp = Test(True)
 
 bus.attach_component(comp)
 bus.read(1, 2)
+
+cbs.unload()
