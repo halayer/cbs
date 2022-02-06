7d6516f18f4f113ead98e54cd80ad5facf93f288
H. A. L <31424568+halayer@users.noreply.github.com>
2022-02-06 11:09:49 +0100

    Bus_detachComponent added

--- bus.c
+++ bus.c
@@ -1,3 +1,20 @@
+#include <stdint.h>
+#include <string.h>
+
+#include "bus.h"
+
+void Bus_attachComponent(Bus *bus, Component *comp) {
+    if (bus->comp_amount == 0) {
+        bus->first = comp;
+    } else {
+        bus->last->next = comp;
+    }
+    
+    bus->last = comp;
+    if (bus->debug) fprintf(bus->debug, "Component 0x%x attached.\n", comp);
+    bus->comp_amount++;
+}
+
 int Bus_detachComponent(Bus *bus, Component *comp) {
     if (bus->comp_amount == 0) goto error;
     if (!comp) goto error;
@@ -29,4 +46,65 @@ int Bus_detachComponent(Bus *bus, Component *comp) {
     if (bus->debug)
         fprintf(bus->debug, "Error occured while detaching component 0x%x from the bus.\n", comp);
     return -1;
+}
+
+Component *Bus_findComponentByAddr(Bus *bus, uint64_t addr) {
+	Component *comp = bus->first;
+	
+	while (1) {
+		if (comp->has_addr_space && comp->is_in_addr_space(comp->inst, addr)) return comp;
+        
+        comp = comp->next;
+	}
+	
+	return NULL;
+}
+
+
+int Bus_read(Bus *bus, uint64_t addr, int byte_size, void *dst) {
+    int ret;
+	Component *comp = Bus_findComponentByAddr(bus, addr);
+	if (!comp) return -1;
+	
+	ret = comp->read(comp->inst, addr, byte_size, dst);
+    
+    if (bus->debug) {
+        if (ret != 0) {
+            fprintf(bus->debug, "Error occured while reading %d byte(s) from 0x%x\n", byte_size, addr);
+            return ret;
+        }
+        
+        if (byte_size <= 8 && dst) {
+            uint64_t buf = 0;
+            memcpy(&buf, dst, (size_t)byte_size);
+            
+            fprintf(bus->debug, "Read %d byte(s) (0x%llx) from 0x%x\n", byte_size, buf, addr);
+        } else {fprintf(bus->debug, "Read %d byte(s) from 0x%x\n", byte_size, addr); }
+    }
+    
+    return ret;
+}
+
+int Bus_write(Bus *bus, uint64_t addr, int byte_size, void *src) {
+    int ret;
+	Component *comp = Bus_findComponentByAddr(bus, addr);
+	if (!comp) return -1;
+	
+	ret = comp->write(comp->inst, addr, byte_size, src);
+    
+    if (bus->debug) {
+        if (ret != 0) {
+            fprintf(bus->debug, "Error occured while writing %d byte(s) to 0x%x\n", byte_size, addr);
+            return ret;
+        }
+        
+        if (byte_size <= 8 && src) {
+            uint64_t buf = 0;
+            memcpy(&buf, src, (size_t)byte_size);
+            
+            fprintf(bus->debug, "Wrote %d byte(s) (0x%llx) from 0x%x\n", byte_size, buf, addr);
+        } else {fprintf(bus->debug, "Wrote %d byte(s) from 0x%x\n", byte_size, addr); }
+    }
+    
+    return ret;
 }
